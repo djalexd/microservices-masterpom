fba4441dfb8d9bbe59967082881432c7569a2c64
alexdobjanschi <alex.dobjanschi@gmail.com>
2016-03-31 07:03:32 +0300

    more plugins for jenkins, seed jobs generated with groovy script

--- vagrant-boxes/plays/roles/jenkins-jobs/templates/add-seed-jobs.groovy
+++ vagrant-boxes/plays/roles/jenkins-jobs/templates/add-seed-jobs.groovy
@@ -3,14 +3,15 @@
  * http://stackoverflow.com/a/28176509/863412
  */
 
-import javaposse.jobdsl.dsl.DslScriptLoader
 import jenkins.model.*
 import hudson.model.*
+import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
+
 import java.util.logging.Logger
 
-def basePath = '/usr/share/jenkins/ref/default-jobs'
-def pattern = '**/*.groovy'
+def basePath = "{{ jobs_location|default('/usr/share/lib/jenkins/jobs') }}"
+def pattern = "{{ jobs_pattern|default('**/*.groovy') }}"
 
 /*
  * project is the seed for default jobs.
@@ -19,16 +20,16 @@ def project = new FreeStyleProject(Jenkins.getInstance(), "Seed job")
 def build = new FreeStyleBuild(project)
 
 def TaskListener taskListener = new hudson.util.LogTaskListener(
-        Logger.getLogger("add-default-jobs"),
-        java.util.logging.Level.INFO)
+		Logger.getLogger("add-default-jobs"),
+		java.util.logging.Level.INFO)
 
 def jm = new JenkinsJobManagement(
-        taskListener.getLogger(),
-        build.getEnvironment(taskListener),
-        build)
+		taskListener.getLogger(),
+		build.getEnvironment(taskListener),
+		build)
 
 new FileNameFinder().getFileNames(basePath, pattern).each { String fileName ->
-    println "\nprocessing file: $fileName"
-    File file = new File(fileName)
-    DslScriptLoader.runDslEngine(file.text, jm)
+	println "\nprocessing file: $fileName"
+	File file = new File(fileName)
+	DslScriptLoader.runDslEngine(file.text, jm)
 }
